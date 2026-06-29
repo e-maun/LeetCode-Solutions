e5b2ca5d31253c94f5c891f20e55578e63807f75
e-maun <emmanuelmaun13@gmail.com>
2026-06-29 13:07:55 +0800

    Update TwoSum.cs

--- Arrays/TwoSum.cs
+++ Arrays/TwoSum.cs
@@ -1,1 +1,23 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            
+            int complement = target - nums[i];
+
+            if(map.ContainsKey(complement))
+            {
+               int[] arr =
+               {
+                map[complement],
+                i
+               };
+               return arr;
+            }
+                map.Add(nums[i], i);
+        }
+        return new int[]  {};
+    } 
+}
