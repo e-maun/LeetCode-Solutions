2b7b15d4b60b9db5ae29cc9a76035511dd8f73df
e-maun <emmanuelmaun13@gmail.com>
2026-07-05 17:54:56 +0800

    Add IsValid method for validating parentheses
    Implement a method to check if parentheses are valid.

--- Stacks/ValidParentheses.cs
+++ Stacks/ValidParentheses.cs
@@ -1,1 +1,43 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new Stack<char>();
 
+foreach (char c in s)
+{
+    //enter the opening brackets into the stack
+    if (c == '(' || c == '[' || c == '{')
+    {
+        stack.Push(c);
+    }
+    //check if the closing brackets match the opening brackets in the stack
+    if (c == ')' || c == ']' || c == '}')
+    {
+        if (stack.Count != 0)
+        {
+            if (c == ')' && stack.Peek() == '(')
+            {
+                stack.Pop();
+            }
+            else if (c == ']' && stack.Peek() == '[')
+            {
+                stack.Pop();
+            }
+            else if (c == '}' && stack.Peek() == '{')
+            {
+                stack.Pop();
+            }
+            else         
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+    return stack.Count == 0;
+        
+    }
+}
